7958bb1609fda4394820701ccf94cd0306feed62
pshukla9653 <88384289+pshukla9653@users.noreply.github.com>
2021-10-03 17:07:55 +0530

    Init

---
+++ pages/users/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <input v-model="userId" type="text"/>
+    <button @click="onLoadUser">submit</button>
+    <p>Please Enter user id</p>
+    <nuxt-child/>
+    
+  </div>
+</template>
+<script>
+export default{
+    data(){
+        return {
+            userId: ''
+        }
+    },
+    methods: {
+        onLoadUser(){
+            this.$router.push('/users/' + this.userId)
+        }
+    }
+
+} 
+</script>
+
